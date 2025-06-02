b311737ba7f598effddb807ac1b9a411b06c61e6
Mani Ivan <ManiIvan@habitable4883.anonaddy.com>
2025-06-03 00:12:31 +0530

    Resolve merge conflicts

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,12 +4,12 @@ import { FaGithub, FaDownload } from 'react-icons/fa';
 import styles from './Hero.module.css';
 
 const cloudsData = [
-  { src: '/src/assets/cloud1.svg', top: '10%', size: 400, opacity: 0.5, speed: 0.02 },
-  { src: '/src/assets/cloud2.svg', top: '30%', size: 500, opacity: 0.7, speed: 0.03 },
-  { src: '/src/assets/cloud3.svg', top: '60%', size: 650, opacity: 0.4, speed: 0.015 },
+  { src: '/cloud1.svg', top: '10%', size: 400, opacity: 0.5, speed: 0.02 },
+  { src: '/cloud2.svg', top: '30%', size: 500, opacity: 0.7, speed: 0.03 },
+  { src: '/cloud3.svg', top: '60%', size: 650, opacity: 0.4, speed: 0.015 },
 
-  { src: '/src/assets/cloud5.svg', top: '90%', size: 500, opacity: 0.6, speed: 0.025 },
-  { src: '/src/assets/cloud6.svg', top: '100%', size: 30, opacity: 0.6, speed: 0.025 },
+  { src: '/cloud5.svg', top: '90%', size: 500, opacity: 0.6, speed: 0.025 },
+  { src: '/cloud6.svg', top: '100%', size: 30, opacity: 0.6, speed: 0.025 },
 ];
 
 const Hero = () => {
